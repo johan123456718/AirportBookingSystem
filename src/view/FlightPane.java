7e1e7b2ce319cf6b49c8f402cd7f0d921751c7bf
Johan Challita <challita@kth.se>
2021-06-21 17:30:55 +0200

    Connect to Db is done

--- src/view/FlightPane.java
+++ src/view/FlightPane.java
@@ -5,13 +5,25 @@
  */
 package view;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import model.bo.Flight;
+import model.db.AirplaneMySQLDb;
 
 /**
  *
@@ -20,24 +32,64 @@ import javafx.scene.layout.VBox;
 public class FlightPane extends VBox{
     
     private MenuBar menuBar;
+    private TableView<Flight> flightTable;
+    private ObservableList<Flight> flightsInTable;
     
-    public FlightPane(){
-        this.init();
+    public FlightPane(AirplaneMySQLDb airplaneDb){
+        final Controller controller = new Controller(airplaneDb, this);
+        this.init(controller);
     }
     
     
-    private void init(){
-        initMenus();
+    private void init(Controller controller){
+        flightsInTable = FXCollections.observableArrayList();
+        initFlightTable();
+        initMenus(controller);
         BorderPane mainPane = new BorderPane();
-        //mainPane.setCenter(booksTable);
+        mainPane.setCenter(flightTable);
         //mainPane.setBottom(bottomPane);
         mainPane.setPadding(new Insets(10, 10, 10, 10));
         this.getChildren().addAll(menuBar, mainPane);
         VBox.setVgrow(mainPane, Priority.ALWAYS);
     }
     
+    private void initFlightTable(){
+        flightTable = new TableView<>();
+        flightTable.setEditable(false);
+        
+        TableColumn<Flight, Integer> flightIdCol = new TableColumn<>("Flight_id");
+        TableColumn<Flight, Integer> airlineIdCol = new TableColumn<>("Airline_id");
+        TableColumn<Flight, String> airlineNameCol = new TableColumn<>("Airline name");
+        TableColumn<Flight, String> fromlocationCol = new TableColumn<>("From location");
+        TableColumn<Flight, String> tolocationCol = new TableColumn<>("To location");
+        TableColumn<Flight, LocalDateTime> depatureTimeCol = new TableColumn<>("Departure time");
+        TableColumn<Flight, LocalDateTime> arrivalTimeCol = new TableColumn<>("Arrival time");
+        TableColumn<Flight, Integer> durationCol = new TableColumn<>("Duration");
+        TableColumn<Flight, Integer> totalSeatsCol = new TableColumn<>("Total seats");
+        
+        flightTable.getColumns().addAll(flightIdCol, airlineIdCol, airlineNameCol, fromlocationCol, tolocationCol, depatureTimeCol, arrivalTimeCol,
+        durationCol, totalSeatsCol);
+        
+        airlineNameCol.prefWidthProperty().bind(flightTable.widthProperty().multiply(0.13));
+        fromlocationCol.prefWidthProperty().bind(flightTable.widthProperty().multiply(0.15));
+        tolocationCol.prefWidthProperty().bind(flightTable.widthProperty().multiply(0.15));
+        depatureTimeCol.prefWidthProperty().bind(flightTable.widthProperty().multiply(0.15));
+        arrivalTimeCol.prefWidthProperty().bind(flightTable.widthProperty().multiply(0.15));
+        
+        flightIdCol.setCellValueFactory(new PropertyValueFactory<>("flightId"));
+        airlineIdCol.setCellValueFactory(new PropertyValueFactory<>("airlineId"));
+        airlineNameCol.setCellValueFactory(new PropertyValueFactory<>("airlineName"));
+        fromlocationCol.setCellValueFactory(new PropertyValueFactory<>("fromLocation"));
+        tolocationCol.setCellValueFactory(new PropertyValueFactory<>("toLocation"));
+        depatureTimeCol.setCellValueFactory(new PropertyValueFactory<>("departureTime"));
+        arrivalTimeCol.setCellValueFactory(new PropertyValueFactory<>("arrivalTime"));
+        durationCol.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        totalSeatsCol.setCellValueFactory(new PropertyValueFactory<>("totalSeats"));
+        flightTable.setItems(flightsInTable);
+    }
     
-    private void initMenus() {
+    
+    private void initMenus(Controller controller) {
 
         Menu fileMenu = new Menu("File");
         MenuItem exitItem = new MenuItem("Exit");
@@ -59,7 +111,27 @@ public class FlightPane extends VBox{
         manageMenu.getItems().addAll(addItem, addRatingItem, addAuthorItem);
         
         
+        connectItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                controller.connect();
+            }
+        });
+        
         menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu, searchMenu, manageMenu);
     }
+    
+    public void displayFlights(List<Flight> flights) {
+        flightsInTable.clear();
+        flightsInTable.addAll(flights);
+    }
+    
+    
+    protected void showAlertAndWait(String msg, Alert.AlertType type) {
+        // types: INFORMATION, WARNING et c.
+        Alert alert = new Alert(type, msg);
+        alert.showAndWait();
+    }
+
 }
